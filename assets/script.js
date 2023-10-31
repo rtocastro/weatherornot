1b73e6afb3926d6e085d6f1857c0f7d75ff03bc4
rtocastro <ricktorres@live.com>
2023-10-30 23:39:32 -0700

    added more html still incomplete

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,65 @@
-var weather = "weather.com/api"
 
-fetch(weather)
-.then(function (response){
+var citysearchbutton = document.getElementById("citySearch")
+var userinput = document.getElementById("city")
 
-return response.json();
-})
+// //Using the 5 Day Weather Forecast API, you'll notice that you will 
+// //need to pass in coordinates instead of just a city name. Using the 
+// //OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+function start(){
 
-.then (function (data) {
-    console.log(data)
+   getCurrentWeather(userinput.value)
+   getForecastWeather(userinput.value)
+}
 
-})
+function getCurrentWeather(cityName){
+
+    var weathercall = "http://api.openweathermap.org/data/2.5/weather?q="+ cityName +"&appid=17991d912ecf9f915449bc8b0469a6f4&units=imperial"
+
+
+
+    fetch(weathercall)
+    .then (function (response){
+    return response.json();
+        
+    }).then (function(data){
+    
+        console.log(data)
+        // access the data and target the elements in html and add the content to the page
+
+    })
+}
+
+function getForecastWeather(cityName){
+
+    var weathercall = "http://api.openweathermap.org/data/2.5/forecast?q="+ cityName +"&appid=17991d912ecf9f915449bc8b0469a6f4"
+
+
+
+    fetch(weathercall)
+    .then (function (response){
+    return response.json();
+        
+    }).then (function(data){
+    
+        console.log(data)
+        for(var i =0; i< 5; i++){
+            console.log(i);
+        }
+    })
+}
+
+
+citysearchbutton.addEventListener("click", start)
+
+
+
+
+//GIVEN a weather dashboard with form inputs
+//WHEN I search for a city
+//THEN I am presented with current and future conditions for that city and that city is added to the search history
+//WHEN I view current weather conditions for that city
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+//WHEN I view future weather conditions for that city
+//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//WHEN I click on a city in the search history
+//THEN I am again presented with current and future conditions for that city
---
+++ readme.md
@@ -0,0 +1,34 @@
+# Current and 5 day forecast
+
+## Description
+
+Just a Very basic app to show the 5 day and current forecast for the weather.
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Just need a browswer that runs HTML 5. (Google Chrome, Firefox, Edge etc
+)
+## Usage
+
+Type in city you would like to see the weather of and get the five day forecast. 
+
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+Documenation from MDN. My Instructor Luigi Campbell. 
+
+
+
